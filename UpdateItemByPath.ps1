ba0114b64c36a235f45cd37fb602aba5ff193391
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-04-01 09:26:29 -0500

    upgrading update item by path
    updates all language versions

--- UpdateItemByPath.ps1
+++ UpdateItemByPath.ps1
@@ -3,10 +3,18 @@
 ######################################################################
 
 # update these: 
-$searchPath = "/sitecore/content/Milwaukee Tool/Products Repository/North America/Safety/Personal Protection Equipment/Vests"
-$template = "{366698BE-EC46-453C-A27D-6CB14DFCEA3D}" # safety gear
-$field = "Configurable"
-$value = "1"
+$searchPath = "/sitecore/content"
+#$template = "{854B9BB8-0081-4B89-A9A5-36CBA093C38D}" # USP hero
+$template = "{E2DDFDFE-B2ED-4ACC-9910-8FD1EA8799C7}" # InnovationsPromo
+#$template = "{65ED3868-2EB3-4C8C-84C6-795E1B8F7570}" #kiosk background container
+$field = "{711288F0-3D69-41A5-8499-20358578AFA3}" # vimeo video id
+$value = ""
+$languageList = @(
+  "en",
+  "en-CA",
+  "es-US",
+  "fr-CA"
+)
 
 ######################################################################
 $StartTime = $(get-date)
@@ -14,16 +22,24 @@ Write-Host "------------------------" -f white
 Write-Host "Begin Script" -f green
 Write-Host "------------------------" -f white
 
-$foundItems = Get-ChildItem $searchPath -Recurse
-
-foreach ($foundItem in $foundItems) {
-	$foundItemTemplate = [Sitecore.Data.Managers.TemplateManager]::GetTemplate($foundItem)
-	if ($foundItemTemplate.InheritsFrom($template)) { 
-		Write-Host "Updating this item: " $foundItem.Name " field: " $field " value: " $value " in this path: " $foundItem.Paths.Path
-		#$foundItem.Editing.BeginEdit()
-		#$foundItem.Fields[$field].Value = $value
-		#$foundItem.Editing.EndEdit()
-	}
+foreach ($language in $languageList) {
+  $foundItems = Get-ChildItem $searchPath -Recurse -Language $language
+  foreach ($foundItem in $foundItems) {
+    $foundItemTemplate = [Sitecore.Data.Managers.TemplateManager]::GetTemplate($foundItem)
+    if ($foundItemTemplate.InheritsFrom($template)) { 
+      New-UsingBlock (New-Object Sitecore.Globalization.LanguageSwitcher $language) {
+        if ($foundItem.Fields[$field].Value -ne $value) {
+          Write-Host "Updating this item: " $foundItem.Name " field: " $field " value: " $value " Language: " $foundItem.Language " in this path: " $foundItem.Paths.Path
+          #$foundItem.Editing.BeginEdit()
+          #$foundItem.Fields[$field].Value = $value
+          #$foundItem.Editing.EndEdit()
+          #Publish-Item -Item $foundItem
+        } else {
+          Write-Host "No Update for this item: " $foundItem.Name " Language: " $foundItem.Language " in this path: " $foundItem.Paths.Path
+        }
+      }
+    }
+  }
 }
 
 $elapsedTime = $(get-date) - $StartTime
