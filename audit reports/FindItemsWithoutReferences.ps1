9dd341d5a3a857888accc5a72ff9d4afd6a056ca
gmorgenneMXG <gmorgenne@themxgroup.com>
2022-08-22 13:16:35 -0500

    adding start/end messages

--- audit reports/FindItemsWithoutReferences.ps1
+++ audit reports/FindItemsWithoutReferences.ps1
@@ -1,7 +1,15 @@
-#############
-# finds items without references
-# attempts delete
-###############
+<#
+    .SYNOPSIS
+        finds items without references
+    .DESCRIPTION
+        uses the link manager to find any references to items, outputs unreferenced items.
+        can toggle audit mode to remove unneeded items
+    .NOTES
+        Version:        1.0
+        Author:         Geoff Morgenne
+#>
+
+$auditMode = $true
 
 function HasReference {
     param(
@@ -13,14 +21,25 @@ function HasReference {
 }
 
 function GetItemsWithoutReferences {
-	$items = Get-ChildItem -Path "master:/sitecore/content/WebKiosk/Home/HD" -Recurse | Where-Object { $_.TemplateName -notmatch "Folder" }
-	#$items = Get-ChildItem -Path "master:/sitecore/content/WebKiosk/Global" -Recurse | Where-Object { $_.TemplateName -notmatch "Folder" }
+	$items = Get-ChildItem -Path "master:/sitecore/content" -Recurse | Where-Object { $_.TemplateName -notmatch "Folder" }
 	foreach($item in $items) {
 		if(!(HasReference($item))) {
 			$item
-			#Remove-Item -Path $item.ItemPath
+            if (!$auditMode) {
+                Remove-Item -Path $item.ItemPath
+            }
 		}
 	}
+}
+
+######################################################################
+$StartTime = $(get-date)
+Write-Host "------------------------" -f white
+Write-Host "Begin Script" -f green
+Write-Host "------------------------" -f white
+
+if ($auditMode) {
+    Write-Host "NOTE: This script is running in audit mode. Update $auditMode bool to act on removing these items."
 }
 
 # output items without links
@@ -34,3 +53,10 @@ GetItemsWithoutReferences | Show-ListView @props -Property @{Label="Name"; Expre
     @{Label="Template ID"; Expression={$_.TemplateID } },
     @{Label="Template Name"; Expression={$_.TemplateName } },
     @{Label="Path"; Expression={$_.ItemPath} }
+
+$elapsedTime = $(get-date) - $StartTime
+$totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
+Write-Host "------------------------" -f white
+Write-Host "End Script - Total time: " $totalTime -f green
+Write-Host "------------------------" -f white
+######################################################################
