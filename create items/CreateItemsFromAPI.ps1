8cfbe0c377854e399cc0be40c63ed76fd113b7b8
gmorgenne <geoff@interactivestrategies.com>
2026-07-17 11:01:57 -0500

    adding basic auth function to create items from api

--- create items/CreateItemsFromAPI.ps1
+++ create items/CreateItemsFromAPI.ps1
@@ -9,8 +9,21 @@ $clientSecret = "secret"
 $domain = "http://cool.site.io"
 $importPath = "/sitecore/content/temp"
 $templateId = "{220FFB90-FFE7-49CD-9A3A-FC71A093A794}"
+$useAccessToken = $false
 
 ######################################################################
+
+function Get-BasicAuth {
+    [CmdletBinding()]
+	param(
+        [Parameter(Mandatory=$true, Position=0)][string] $clientId,
+        [Parameter(Mandatory=$true, Position=1)][string] $clientSecret
+	)
+
+    $pair = "$($clientId):$($clientSecret)"
+    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+    return "Basic $encodedCreds"
+}
 
 function Get-AccessToken {
     [CmdletBinding()]
@@ -21,19 +34,20 @@ function Get-AccessToken {
 	)
     $tokenUri = "{0}/token?grant_type=client_credentials&client_id={1}&client_secret={2}" -f $domain, $clientId, $clientSecret
     $tokenResponse = Invoke-WebRequest -uri $tokenUri -Method Post -UseBasicParsing
-    return ($tokenResponse | ConvertFrom-Json).access_token
+    $accessToken = $($tokenResponse | ConvertFrom-Json).access_token
+    return "Bearer $accessToken"
 }
 
 function Get-ProductData {
     [CmdletBinding()]
 	param(
         [Parameter(Mandatory=$true, Position=0)][string] $domain,
-		[Parameter(Mandatory=$true, Position=1)][string] $accessToken
+		[Parameter(Mandatory=$true, Position=1)][string] $authorization
 	)
 
     $productUri = "{0}/api/products" -f $domain
     $headers = @{
-        Authorization = "Bearer {0}" -f $accessToken
+        Authorization = $authorization
     }
     $productResponse = Invoke-WebRequest -uri $productUri -Headers $headers -Method Get -UseBasicParsing
     return $productResponse | ConvertFrom-Json
@@ -46,9 +60,14 @@ Write-Host "------------------------" -f white
 Write-Host "Begin Script" -f green
 Write-Host "------------------------" -f white
 
-$accessToken = Get-AccessToken $domain $clientId $clientSecret
-
-$products = Get-ProductData $domain $accessToken
+$authorization = ""
+if ($useAccessToken) {
+    $authorization = Get-AccessToken $domain $clientId $clientSecret
+    
+} else {
+    $authorization = Get-BasicAuth $clientId $clientSecret
+}
+$products = Get-ProductData $domain $authorization
 
 New-UsingBlock (New-Object Sitecore.Data.BulkUpdateContext) {
     foreach ($product in $products) {
